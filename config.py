2480cb35d32b998ddb1f5a5ed98e13725c87b5d7
michaelwu317 <40549054+michaelwu317@users.noreply.github.com>
2022-06-10 19:02:44 -0700

    added config file containing keys

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import alpaca_trade_api as tradeapi
 
+#keys definition
 public_key = '' #protected
 secret_key = '' #protected
 api_url = 'https://paper-api.alpaca.markets' 
-#api = tradeapi.REST(key_id= public_key, secret_key=secret_key, base_url=api_url) 
-
-while True:
-    x = 1
-    print(x)
+api = tradeapi.REST(key_id= public_key, secret_key=secret_key, base_url=api_url) 
