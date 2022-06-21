f10ef39bc5cc919beb5247973bd6d00b09f8c377
michaelwu317 <40549054+michaelwu317@users.noreply.github.com>
2022-06-20 22:04:57 -0700

    more url setup

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ account_url = "{}/v2/accounts".format(base_url)
 orders_url = "{}/v2/orders".format(base_url)
 #r = requests.get
 #continuous block
+def get_accounts():
+    r = requests.get(account_url, headers = {'APCA-API-KEY-ID': public_key, 'APCA-API-SECRET-KEY': secret_key})
 while True:
     x = 1
     
