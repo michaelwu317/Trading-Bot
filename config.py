4815c115d28096ea4dab3978ad035d0f9cc9d095
michaelwu317 <40549054+michaelwu317@users.noreply.github.com>
2022-07-02 14:48:55 -0700

    added create order functions

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import alpaca_trade_api as tradeapi
 #keys definition
 public_key = '' #protected
 secret_key = '' #protected
-api_url = 'https://paper-api.alpaca.markets' 
-api = tradeapi.REST(key_id= public_key, secret_key=secret_key, base_url=api_url) 
+api_url =  'https://paper-api.alpaca.markets'
+endpointurl = 'https://paper-api.alpaca.markets'
+api = tradeapi.REST(public_key, secret_key, api_url, api_version='v2') 
+headers = {'APCA-API-KEY-ID': public_key, 'APCA-API-SECRET-KEY': secret_key}
--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import alpaca_trade_api as tradeapi
+import json
 from config import *
 import requests
+import logging
+import pandas
 
-base_url = "https://paper-api.alpaca.markets"
+
+base_url = 'https://paper-api.alpaca.markets'
 account_url = "{}/v2/accounts".format(base_url)
 orders_url = "{}/v2/orders".format(base_url)
 #r = requests.get
 #continuous block
-def get_accounts():
-    r = requests.get(account_url, headers = {'APCA-API-KEY-ID': public_key, 'APCA-API-SECRET-KEY': secret_key})
-while True:
-    x = 1
-    
+#logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+#def get_accounts():
+#r = requests.get(account_url, headers = {'APCA-API-KEY-ID': public_key, 'APCA-API-SECRET-KEY': secret_key})
+account = api.get_account()
+
+def create_order(symbol, qty, side, type, time_in_force):
+    data = {
+        "symbol":symbol,
+        "qty":qty,
+        "side":side,
+        "type":type,
+        "time_in_force": time_in_force
+    } 
+    r = requests.post(orders_url, json=data, headers=headers)
+    return json.loads(r.content)
+
+print(aapl.df)
+
+print(account)
+
